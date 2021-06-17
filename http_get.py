d3e8b5d95b2a808c356bcdbb06d3000e481107f7
marektomka <62624961+marektomka@users.noreply.github.com>
2021-06-17 18:10:14 +0200

    úpravy v cykloch pre hlavicky,obsah... vypisuje: TypeError: 'NoneType' object is not subscriptable

--- http_get.py
+++ http_get.py
@@ -7,73 +7,85 @@ import re
 types=['http', 'https'] 
 header=""
 content=""
-codes=[301, 302, 303, 307, 308]
+hostname=""
+codes=[301, 302, 303, 307, 308] 
 
-URL=str(sys.argv[1])
-spl=re.match('[a-z]*',URL)
-typ=spl[0]
-URL=URL.replace(typ+"://","")
-spl=re.match('([\w\-\.]+)',URL)
-hostname=spl[0]
-path=URL.replace(hostname,"")
-if path=="":
-    path="/"
+URL=(sys.argv[1])
 
-if (typ in types):
+while True:
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    if typ == types[1]:
+    headers={} 
+    spl=re.match('[a-z]*',URL)
+    typ=spl[0]
+    URL=URL.replace(typ+"://","")
+    spl=re.match('([\w\-\.]+)',URL)
+    hostname=spl[0]
+    path=URL.replace(hostname,"")
+    if path=="":
+        path="/"
+    
+    if typ=='https':
         s.connect((hostname,443))
         s=ssl.wrap_socket(s)
     else:
         s.connect((hostname,80))
-    while True:
-        f=s.makefile("rwb")
-        f.write(f'GET {path} HTTP/1.1\r\n'.encode("ASCII"))
-        f.write(f'Host: {hostname}\r\n'.encode("ASCII"))
-        f.write(f'Accept-charset: UTF-8\r\n\r\n'.encode("ASCII"))
-        f.flush()
-        line=f.readline().decode("ASCII")
-        l=line.split(" ")
-        status_code=l[1]
-        status_message=l[2]
-        headers={}
-        l=f.readline().decode("ASCII").strip()
-        while l != "":
-            header=l.strip().split(':',1)
-            if len(l)==2:
-                header=l[0].lower()
-                content=d[1].lower()
-                headers[header]=content
-            l=f.readline().decode('ASCII')
-            if (status_code in codes):
-                URL=headers['location']
 
-                f.close()
-                s.close()
-            elif (status_code=="200"):
-                break
-            else:
-                sys.stderr.write(f'{status_code} {status_message}')
-                f.close()
-                sys.exit(1)
-                break
-        if status_code=="200":
-            for h in headers:
-                if h=="content-length":
-                    length=int(headers["content-length"])
-                    content=f.read(length).decode("ASCII")
-                    sys.stdout.buffer.write(content)
-                    break
-                elif h=="transfer-encoding":
-                    while True:
-                        le=f.readline()
-                        length=int(le,16)
-                        content=f.read(length)
-                        sys.stdout.buffer.write(content)
-                        if length==0:
-                            break
-                        f.readline()
-
+    f=s.makefile("rwb")
+    f.write(f'GET {path} HTTP/1.1\r\n'.encode("ASCII"))
+    f.write(f'Host: {hostname}\r\n'.encode("ASCII"))
+    f.write(f'Accept-charset: UTF-8\r\n\r\n'.encode("ASCII"))
     f.flush()
-    f.close()
-    sys.exit(0)
+
+    #status
+    line=f.readline().decode("ASCII")
+    l=line.split(" ")
+    status_code=l[1]
+    status_message=l[2]
+    
+    #hlavicky - cyklus az po \r\n
+    line=f.readline().decode("ASCII").strip()
+    while line != '\r\n':
+        l=line.split(':',1)
+        if len(l)==2:
+            header=l[0].lower()
+            content=l[1].lower()
+        headers[header]=content
+        line=f.readline().decode('ASCII')
+        
+    if (status_code == '301' or status_code == '302' or status_code == '303' or status_code == '307' or status_code == '308'):
+        URL=headers['location']
+        f.close()
+        s.close()
+    elif (status_code=="200"):
+        break
+    else:
+        sys.stderr.write(f'404 Not Found\n')
+        f.close()
+        sys.exit(0)
+        break
+#obsah
+if status_code=="200":
+    for h in headers:
+        if h=="content-length":
+            length=int(headers["content-length"])
+            content=f.read(length).decode('ASCII')
+            sys.stdout.buffer.write(content)
+            break
+                
+        elif h=="transfer-encoding":
+            while True:
+                le=f.readline().decode('ASCII')
+                length=int(le,16)
+                content=f.read(length)
+                sys.stdout.buffer.write(content)
+                if lenght==0:
+                    break
+                f.readline()
+else:
+    print('Chybne URL')
+
+f.flush()
+f.close()
+s.close()
+sys.exit(0)
+
